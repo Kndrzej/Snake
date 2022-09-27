8afedbe65c72a476439f75235d18c6ff395774e9
Kndrzej <kozinskiandrzej4@gmail.com>
2022-09-27 20:57:35 +0200

    Update PlayerMovement.cs
    Player no longer can self-collide in straight line

--- Assets/_GameAssets/Scripts/PlayerMovement.cs
+++ Assets/_GameAssets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private KeyCode buttonDown = KeyCode.S;
     [SerializeField] private KeyCode buttonLeft = KeyCode.A;
     [SerializeField] private KeyCode buttonRight = KeyCode.D;
-    
+
     private Vector3 direction = Vector3.right;
     private PlayerBody playerBody;
     private void Start()
@@ -25,24 +25,24 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(buttonUp))
         {
-            direction = Vector3.up;
+            if (direction != Vector3.down) direction = Vector3.up;
         }
-        else if(Input.GetKeyDown(buttonDown))
+        else if (Input.GetKeyDown(buttonDown))
         {
-            direction = Vector3.down;
+            if (direction != Vector3.up) direction = Vector3.down;
         }
         else if (Input.GetKeyDown(buttonLeft))
         {
-            direction = Vector3.left;
+            if (direction != Vector3.right) direction = Vector3.left;
         }
         else if (Input.GetKeyDown(buttonRight))
         {
-            direction = Vector3.right;
+            if (direction != Vector3.left) direction = Vector3.right;
         }
     }
     private void FixedUpdate()
     {
-        for(int i = playerBody.segments.Count-1 ;i > 0; i--)
+        for (int i = playerBody.segments.Count - 1; i > 0; i--)
         {
             playerBody.segments[i].position = playerBody.segments[i - 1].position;
         }
